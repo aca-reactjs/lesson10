39aa9e74f58dd453096ebf602534db39cc4a7b93
sargis <abovyan.sargis90@gmail.com>
2022-03-12 12:37:17 +0400

    add custom hook with context

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,16 @@
-import { useState } from "react";
+import CalculatorProvider from "./Providers/Calculator.provider";
+import Controllers from "./components/Controllers";
+import Screen from "./components/Screen";
+
 import styles from "./app.module.css";
 
-import useCalculator from "./hooks/useCalculator.hook";
-
 function App() {
-  const [inputValue, setInputValue] = useState(1);
-
-  const { decrement, increment, state } = useCalculator();
-
-  const handleInputChange = (e) => {
-    setInputValue(Number(e.target.value));
-  };
-
-  const handlePlusButtonClick = () => {
-    increment(inputValue);
-  };
-  const handleMinusButtonClick = () => {
-    decrement(inputValue);
-  };
-
   return (
     <div className={styles.app}>
-      <div className={styles.screen}>{state}</div>
-      <div className={styles.buttonWrapper}>
-        <button onClick={handlePlusButtonClick} className={styles.button}>
-          +
-        </button>
-        <input
-          type="number"
-          className={styles.input}
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-        <button onClick={handleMinusButtonClick} className={styles.button}>
-          -
-        </button>
-      </div>
+      <CalculatorProvider>
+        <Screen />
+        <Controllers />
+      </CalculatorProvider>
     </div>
   );
 }
