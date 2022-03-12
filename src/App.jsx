44e8b6514ed3dc1c320fd4a2d70fc535933ad0cd
sargis <abovyan.sargis90@gmail.com>
2022-03-12 10:23:01 +0400

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+// import styles from "./app.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.mainHeader}>hello</h1>
+    </div>
+  );
+}
+
+export default App;
